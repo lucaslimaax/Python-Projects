61b80317bd253208cdf37888b35b1a00e97bfc7b
Lucas-trabalho <lucashendall@gmail.com>
2019-09-05 10:46:17 -0300

    final version

--- photo-organizer.py
+++ photo-organizer.py
@@ -3,24 +3,35 @@ import shutil
 from datetime import datetime
 from PIL import Image
 
-def folder_path_from_photo_date(file):
-    date=photo_shooting_date(file)
-    return date.strftime('%Y')+'/' +date.strftime('%Y-%m-%d')
+class PhotoOrganizer:
+    extensions = ['jpg', 'jpeg', 'JPG', 'JPEG']
 
-def photo_shooting_date(file):
-    photo = Image.open(file)
-    info = photo._getexif()
-    if 36867 in info:
-        date = info[36867]
-        date = datetime.strptime(date, '%Y:%m:%d %H:%M:%S')
-    else:
-        date = datetime.fromtimestamp(os.path.getmtime(file))
-    return date
+    def folder_path_from_photo_date(self,file):
+        date=self.photo_shooting_date(file)
+        return date.strftime('%Y')+'/' +date.strftime('%Y-%m-%d')
 
-def move_photo(file):
-    new_folder=folder_path_from_photo_date(file)
-    if not os.path.exists(new_folder):
-        os.makedirs(new_folder)
+    def photo_shooting_date(self,file):
+        photo = Image.open(file)
+        info = photo._getexif()
+        if 36867 in info:
+            date = info[36867]
+            date = datetime.strptime(date, '%Y:%m:%d %H:%M:%S')
+        else:
+            date = datetime.fromtimestamp(os.path.getmtime(file))
+        return date
+
+    def move_photo(self,file):
+        new_folder=self.folder_path_from_photo_date(file)
+        if not os.path.exists(new_folder):
+            os.makedirs(new_folder)
         shutil.move(file, new_folder+'/'+file)
 
-print(folder_path_from_photo_date('foto.jpg'))
+    def organize(self):
+        photos = [
+        filename for filename in os.listdir('.') if any(filename.endswith(ext) for ext in self.extensions)
+        ]
+        for filename in photos:
+            self.move_photo(filename)
+
+PO = PhotoOrganizer()
+PO.organize()
